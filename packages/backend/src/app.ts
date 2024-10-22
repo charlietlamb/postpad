74adf89462a8323945e574ef79ef9039ab16eb5a
Charlie Lamb <charlielamb20@icloud.com>
2024-10-22 19:26:56 +0100

    init a bit late...

---
+++ packages/backend/src/app.ts
@@ -0,0 +1,16 @@
+import createApp from '@/lib/create-app'
+import configureOpenAPI from '@/lib/configure-open-api'
+import index from '@/routes/index.route'
+import users from '@/routes/user/user.index'
+import connects from '@/routes/connects/connects.index'
+const app = createApp()
+
+const routes = [index, users, connects]
+
+configureOpenAPI(app)
+
+routes.forEach((route) => {
+  app.route('/', route)
+})
+
+export default app
---
+++ packages/backend/src/db/schema/index.ts
@@ -0,0 +1,6 @@
+export * from './accounts'
+export * from './authenticators'
+export * from './connects'
+export * from './sessions'
+export * from './users'
+export * from './verification-tokens'
